ce7712e1bba5b6197b480ebf6fc04f35d41b88ec
Tom Ramos <tom.ramos@gbs-it.de>
2024-10-28 10:43:40 +0100

    added the class Spiel.java

---
+++ backend/src/main/java/data/Spiel.java
@@ -0,0 +1,50 @@
+package data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Spiel {
+    private final List<Spieler> spielerList;
+    private final LocalDateTime datum;
+    private final UUID uuid;
+    private int startScore;
+
+    public Spiel(List<Spieler> spielerList, int startScore, boolean dobbleOut){
+        this.spielerList = spielerList;
+        this.datum = LocalDateTime.now();
+        this.uuid = UUID.randomUUID();
+        this.startScore = startScore;
+        this.doubleOut = dobbleOut;
+    }
+
+    public List<Spieler> getSpielerList() {
+        return spielerList;
+    }
+
+    public LocalDateTime getDatum() {
+        return datum;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public int getStartScore() {
+        return startScore;
+    }
+
+    public void setStartScore(int startScore) {
+        this.startScore = startScore;
+    }
+
+    public boolean isDoubleOut() {
+        return doubleOut;
+    }
+
+    public void setDoubleOut(boolean doubleOut) {
+        this.doubleOut = doubleOut;
+    }
+    private boolean doubleOut;
+}
